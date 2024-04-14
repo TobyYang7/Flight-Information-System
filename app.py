558224fcd05ce4567821e21e30e404e35fce1e75
TobyYang7 <tobyyang7@foxmail.com>
2024-04-15 02:05:58 +0800

    admin & user

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 
 # database name
-db_path = 'database/database.db'
+db_path = 'database/test.db'
 
 # config app to use database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
@@ -19,48 +19,16 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
 
-# login
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-
-# schema of database
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
-
-
-class RegisterForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
-    submit = SubmitField('Register')
-
-    def validate_username(self, username):
-        existing_user_username = User.query.filter_by(
-            username=username.data).first()
-        if existing_user_username:
-            raise ValidationError(
-                'That username already exists. Please choose a different one.')
+# define admin and user
+ADMIN_ID = 'admin'
+ADMIN_PASSWORD = 'admin1234'
+USER_ID = 'user'
+USER_PASSWORD = 'user1234'
 
 
 class LoginForm(FlaskForm):
-    username = StringField(validators=[
-                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
-
-    password = PasswordField(validators=[
-                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
-
+    id = StringField('ID', validators=[InputRequired(), Length(min=4, max=15)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
     submit = SubmitField('Login')
 
 
@@ -69,43 +37,36 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            if bcrypt.check_password_hash(user.password, form.password.data):
-                login_user(user)
-                return redirect(url_for('dashboard'))
-    return render_template('login.html', form=form)
-
-
-@app.route('/dashboard', methods=['GET', 'POST'])
-@login_required
+@app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
 
-@app.route('/logout', methods=['GET', 'POST'])
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
+@app.route('/wrong')
+def wrong():
+    return render_template('wrong.html')
 
 
-@ app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegisterForm()
-
+@app.route('/admin_login', methods=['GET', 'POST'])
+def admin_login():
+    form = LoginForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('login'))
+        if form.id.data == ADMIN_ID and form.password.data == ADMIN_PASSWORD:
+            return redirect(url_for('dashboard'))
+        else:
+            return redirect(url_for('wrong'))
+    return render_template('admin_login.html', form=form)
 
-    return render_template('register.html', form=form)
+
+@app.route('/user_login', methods=['GET', 'POST'])
+def user_login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.id.data == USER_ID and form.password.data == USER_PASSWORD:
+            return redirect(url_for('dashboard'))
+        else:
+            return redirect(url_for('wrong'))
+    return render_template('user_login.html', form=form)
 
 
 if __name__ == "__main__":
