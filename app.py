1217f7cd376bd562289b52e05616a18b27a67c44
TobyYang7 <tobyyang7@foxmail.com>
2024-04-21 23:23:39 +0800

    port

--- app.py
+++ app.py
@@ -144,5 +144,5 @@ def admin_page():
     return render_template('user_page.html', history=history)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
